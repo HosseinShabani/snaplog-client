dc73fb04b642c9c1ea18d18b3e50d1d0374de0ea
Hossein Shabani <hosseinshabani.work@gmail.com>
2024-08-10 06:30:14 +0330

    Stop recording question

--- components/Recorder.web.tsx
+++ components/Recorder.web.tsx
@@ -5,6 +5,16 @@ import { toast } from 'burnt';
 import { cn, getMMSSFromMillis } from '@/lib/utils';
 import { PauseIcon, PlayIcon } from '@/lib/icons';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 type RecorderProps = {
   onRecordFinish: (name: string, blob: Blob) => void;
@@ -13,6 +23,7 @@ type RecorderProps = {
 const Recorder: React.FC<RecorderProps> = ({ onRecordFinish }) => {
   const [isRecording, setIsRecording] = React.useState<boolean>(false);
   const [paused, setPaused] = React.useState<boolean>(false);
+  const [askStop, setAskStop] = React.useState<boolean>(false);
   const [time, setTime] = React.useState<number>(0);
   const mediaRecorderRef = React.useRef<MediaRecorder | null>(null);
   const audioChunksRef = React.useRef<Blob[]>([]);
@@ -90,43 +101,69 @@ const Recorder: React.FC<RecorderProps> = ({ onRecordFinish }) => {
   }
 
   return (
-    <View className="flex flex-row justify-between items-center p-2 border border-dashed border-zinc-400 rounded-lg shadow-lg shadow-gray-300/50 bg-white">
-      <View className="flex flex-row items-center gap-2.5">
-        <Pressable
-          onPress={isRecording ? stopRecording : startRecording}
-          className={cn(
-            'bg-black border-2 border-black px-3 py-2 rounded-md gap-2.5 flex flex-row items-center',
-            {
-              'bg-white': isRecording,
-            },
-          )}
-        >
-          <View
-            className={cn('w-3.5 h-3.5 bg-red rounded-full', {
-              'rounded-sm': isRecording,
-            })}
-          />
-          <Text
-            className={cn('typo-[16-400] text-white', {
-              'text-black': isRecording,
-            })}
-          >
-            {isRecording ? 'Stop' : 'Start'}
-          </Text>
-        </Pressable>
-        {isRecording && (
-          <Button variant="ghost" className="p-1 h-auto flex-row" onPress={pauseRecording}>
-            {!paused ? (
-              <PauseIcon size={16} className="text-black " />
-            ) : (
-              <PlayIcon size={16} className="text-black mr-0.5" />
+    <Dialog open={askStop}>
+      <View className="flex flex-row justify-between items-center p-2 border border-dashed border-zinc-400 rounded-lg shadow-lg shadow-gray-300/50 bg-white">
+        <View className="flex flex-row items-center gap-2.5">
+          <Pressable
+            onPress={() => {
+              isRecording ? setAskStop(true) : startRecording();
+            }}
+            className={cn(
+              'bg-black border-2 border-black px-3 py-2 rounded-md gap-2.5 flex flex-row items-center',
+              {
+                'bg-white': isRecording,
+              },
             )}
-            <Text className="typo-[14-500]">{!paused ? 'Pause' : 'Resume'}</Text>
-          </Button>
-        )}
+          >
+            <View
+              className={cn('w-3.5 h-3.5 bg-red rounded-full', {
+                'rounded-sm': isRecording,
+              })}
+            />
+            <Text
+              className={cn('typo-[16-400] text-white', {
+                'text-black': isRecording,
+              })}
+            >
+              {isRecording ? 'Stop' : 'Start'}
+            </Text>
+          </Pressable>
+          <DialogContent className="bg-white sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="typo-[18-500] leading-snug">
+                Are you sure you want to stop the recording?
+              </DialogTitle>
+            </DialogHeader>
+            <DialogFooter className="flex flex-row">
+              <Button variant="destructive" size="sm" onPress={() => setAskStop(false)}>
+                <Text className="typo-[14-400] text-gray-80">No</Text>
+              </Button>
+              <Button
+                variant="default"
+                size="sm"
+                onPress={() => {
+                  setAskStop(false);
+                  stopRecording();
+                }}
+              >
+                <Text className="typo-[14-400] text-white">Yes</Text>
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+          {isRecording && (
+            <Button variant="ghost" className="p-1 h-auto flex-row" onPress={pauseRecording}>
+              {!paused ? (
+                <PauseIcon size={16} className="text-black " />
+              ) : (
+                <PlayIcon size={16} className="text-black mr-0.5" />
+              )}
+              <Text className="typo-[14-500]">{!paused ? 'Pause' : 'Resume'}</Text>
+            </Button>
+          )}
+        </View>
+        <Text className="typo-[16-400] text-black/50">{getMMSSFromMillis(time ?? 0)}</Text>
       </View>
-      <Text className="typo-[16-400] text-black/50">{getMMSSFromMillis(time ?? 0)}</Text>
-    </View>
+    </Dialog>
   );
 };
 
